689710bb1249f78113398126625b420d481e5221
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-07-25 16:16:49 -0700

    Create AssemblyInfo.cs

---
+++ texture-pack-app/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("Resource Pack Creator for Minecraft")]
+[assembly: AssemblyDescription ("The 'Resource Pack Creator for Minecraft' is a simple GUI application, creating a pack.mcmeta and folder as well as extracting the vanilla assets for you.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("Resource Pack Creator for Minecraft")]
+[assembly: AssemblyCopyright ("Vortex")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.3.1.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
