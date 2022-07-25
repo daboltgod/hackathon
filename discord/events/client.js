31af74015c831b3f6ea86ac3f5cae8fd219ae3da
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-07-25 12:57:45 -0700

    Create client.js

---
+++ discord/events/client.js
@@ -0,0 +1,4 @@
+let config = require(`${process.cwd()}/botconfig/config.json`)
+const Discord = require("discord.js")
+const moment = require("moment")
+const { nFormatter } = require(`${process.cwd()}/handlers/functions`)
