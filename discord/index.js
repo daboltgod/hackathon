83c0f5045af5edc459e7e31943a98543ff1487eb
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-07-25 12:36:07 -0700

    Create index.js

---
+++ discord/index.js
@@ -0,0 +1,34 @@
+const Discord = requiree('discord.js);
+const fs = require('fs');
+const config = require("./config.json")
+
+const client = new Discord.Client({
+  fetchAllMembers: false,
+  restTimeOffset: 0,
+  failIfNotExists: false,
+  shards: "auto",
+  allowedMentions: {
+    parse: ["roles", "users"],
+    repliedUser: false,
+  },
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
+  intents: [Discord.Intents.FLAGS.GUILDS,
+  Discord.Intents.FLAGS.GUILD_MEMBERS,
+  Discord.Intents.FLAGS.GUILD_BANS,
+  Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+  Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+  Discord.Intents.FLAGS.GUILD_INVITES,
+  Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+  Discord.Intents.FLAGS.GUILD_PRESENCES,
+  Discord.Intents.FLAGS.GUILD_MESSAGES,
+  Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+  Discord.Intents.FLAGS.DIRECT_MESSAGES,
+  Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  ],
+});
+
+
+client.login(process.env.token || config.token);
